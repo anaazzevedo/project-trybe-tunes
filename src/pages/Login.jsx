951eef7c8b500b24f71ca6468eb729245f772fe8
Ana <anateixeirazevedo@gmail.com>
2022-11-04 11:20:44 -0300

    Requisito 1, feito

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <button type="button">Login</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
