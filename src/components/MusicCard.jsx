eb10209671e99fd6eae8e7d1cc0c601571ba33e9
Ana <anateixeirazevedo@gmail.com>
2022-11-15 16:19:49 -0300

    Requisito 9, feito

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
-import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
   state = {
     checkFav: false,
     loading: false,
+    favoritesMusics: [],
   };
+
+  async componentDidMount() {
+    const fav = await getFavoriteSongs();
+    this.setState({
+      favoritesMusics: fav,
+    });
+  }
 
   async favorites({ target }, musics) {
     this.setState({
@@ -29,7 +37,7 @@ class MusicCard extends Component {
   }
 
   render() {
-    const { checkFav, loading } = this.state;
+    const { checkFav, loading, favoritesMusics } = this.state;
     const { trackName, previewUrl, trackId, musics } = this.props;
     return (
       <div className="targetAudio">
@@ -53,7 +61,8 @@ class MusicCard extends Component {
             data-testid={ `checkbox-music-${trackId}` }
             name="fav"
             onChange={ (e) => this.favorites(e, musics) }
-            checked={ checkFav }
+            checked={ !checkFav
+              ? favoritesMusics.some((song) => song.trackId === trackId) : checkFav }
           />
         </label>
       </div>
